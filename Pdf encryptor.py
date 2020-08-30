bd42cdd462f4b1b53796b2e87324ff24312f57e6
Lokesh <44214988+LokeshMaradi@users.noreply.github.com>
2020-08-30 18:32:18 +0530

    Update Pdf encryptor.py

--- Pdf encryptor.py
+++ Pdf encryptor.py
@@ -4,3 +4,4 @@ writer_obj=PdfFileWriter()
 pdf=open("C:\\Users\\pdf_for_enc.pdf",'wb')
 #to encrypt a pdf
 writer_obj.encrypt("pass1","pass2",True) #pass1 and pass2 are for viewing and writing authorizations respectively
+writer_obj.(pdf)
