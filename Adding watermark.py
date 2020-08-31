0704005203493114acca439bd0ee642449a231b2
Lokesh <44214988+LokeshMaradi@users.noreply.github.com>
2020-08-31 20:09:02 +0530

    Create Adding watermark.py
    This program helps us to add a water mark or something to embed in all the existing pages in a pdf

---
+++ Adding watermark.py
@@ -0,0 +1,13 @@
+from PyPDF2 import PdfFileWriter,PdfFileReader
+read1=PdfFileReader("C:\\Users\\pdf1.pdf")
+read2=PdfFileReader("C:\\Users\\pdf2.pdf")
+page_w=read2.getPage(3)#this is the page where the logo is there, for me lets assume 3rd page
+n=read1.getNumPages()
+writer_obj1=PdfFileWriter()
+for j in range(n):
+	page=read1.getPage(j) #Every page will be read here
+	page.mergePage(page_w) #Every page will be merged here
+	writer_obj1.addPage(page) #Added to writer object
+pdf1=open("C:\\Users\\pdfwatermark.pdf",'wb')
+writer_obj1.write(pdf1) #Added to final pdf
+pdf1.close()
